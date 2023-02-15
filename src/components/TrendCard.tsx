8419e79b6ce6a29abba4caec20230c9477ffe03d
hamidrezahashemi71 <hh7192@gmail.com>
2023-02-15 06:06:53 +0330

    trends dynamic ui

--- src/components/TrendCard.tsx
+++ src/components/TrendCard.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-
-const TrendCard = () => {
+const TrendCard = (props: any) => {
+  const {icon, abrv, name, price, diff, chart} = props
   return (
     <div className='w-[297px] bg-gray-900 rounded-2xl px-7 py-[18px]'>
       <div className='flex flex-col gap-[35px]'>
         <div className='flex justify-between items-center'>
           <div className='flex justify-between items-center gap-[14px]'>
-            <img src="images/bitcoin.png" alt="Rodi" />
-            <p className='font-raleway font-semibold text-[white] text-sm'>BTC</p>
-            <span className='bg-[#C6C6C6] w-full py-1 px-[6px] text-lg cursor-pointer hover:bg-gray-600 rounded-[4px] hover:text-white transition-all duration-200s'>BITCOIN</span>
+            <img src={icon} alt="Rodi" />
+            <p className='font-raleway font-semibold text-[white] text-sm'>{abrv}</p>
+            <span className='w-full bg-[#C6C6C6] py-1 px-[6px] text-sm cursor-pointer hover:bg-gray-600 rounded-[4px] hover:text-white transition-all duration-200s'>{name}</span>
           </div>
           <div className='w-[34px] h-[34px] rounded-full bg-gray-500 flex justify-center items-center hover:scale-110 transition-all duration-200 cursor-pointer '>
             <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -18,10 +17,10 @@ const TrendCard = () => {
         </div>
         <div className='flex justify-between'>
           <div className='flex flex-col'>
-            <p className='font-roboto font-medium text-2xl text-white'>$56,624.54</p>
-            <p className='font-roboto font-medium text-lg text-[#B6B6B6]'>1.41%</p>
+            <p className='font-roboto font-medium text-2xl text-white'>${price}</p>
+            <p className='font-roboto font-medium text-lg text-[#B6B6B6]'>{diff}%</p>
           </div>
-          <img src="images/chart-state.png" alt="chart" />
+          <img src={chart} alt="chart" />
         </div>
       </div>
     </div>
